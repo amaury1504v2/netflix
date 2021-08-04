42707b6605a45414a28e39aba61c5b22330ac3d6
Amaury Gimonnet <amaury.gimonnet@edu.devinci.fr>
2021-08-04 14:51:14 +0200

    HomeScreen UI

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -5,12 +5,16 @@ import { Text, View } from '../../components/Themed';
 import categories from '../../assets/data/categories';
 import HomeCategory from '../../components/HomeCategory';
 
-const firstCategory = categories.items[0]
+const firstCategory = categories.items[2]
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
         <HomeCategory category={firstCategory} />
+        <FlatList 
+            data = {categories.items}
+            renderItem = {({item}) => <HomeCategory category={item}/>} 
+            />
     </View>
   );
 }
